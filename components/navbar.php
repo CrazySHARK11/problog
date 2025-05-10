07055d9f4f3b2061af10537dd7daef393eef6108
Lovenish <heelme1181@gmail.com>
2025-05-10 15:00:34 +0530

    fixi : navbar url

--- components/navbar.php
+++ components/navbar.php
@@ -20,26 +20,26 @@
                         <a href="<?php echo $basePath; ?>" class="nav-link fw-medium" style="color: #2e384d;">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a href="<?php echo $basePath; ?>about.php" class="nav-link fw-medium" style="color: #2e384d;">About</a>
+                        <a href="<?php echo $basePath; ?>about" class="nav-link fw-medium" style="color: #2e384d;">About</a>
                     </li>
                     <li class="nav-item">
-                        <a href="<?php echo $basePath; ?>blogs.php" class="nav-link fw-medium" style="color: #2e384d;">Blogs</a>
+                        <a href="<?php echo $basePath; ?>blogs" class="nav-link fw-medium" style="color: #2e384d;">Blogs</a>
                     </li>
                     <li class="nav-item">
-                        <a href="<?php echo $basePath; ?>contact.php" class="nav-link fw-medium" style="color: #2e384d;">Contact us</a>
+                        <a href="<?php echo $basePath; ?>contact" class="nav-link fw-medium" style="color: #2e384d;">Contact us</a>
                     </li>
                 </ul>
 
                 <!-- Auth Links -->
                 <div class="d-flex auth mt-2 mt-md-0">
                     <?php if (isset($_SESSION['user_id'])) { ?>
-                        <a href="<?php echo $basePath; ?>user/profile.php" class="text-decoration-none fw-medium" style="color: #607f61;">
+                        <a href="<?php echo $basePath; ?>user/profile" class="text-decoration-none fw-medium" style="color: #607f61;">
                             <img width="45" height="45" class="rounded-circle object-fit-cover" src="<?php echo $basePath . "uploads/" . htmlspecialchars($user['profile_picture']); ?>" alt="">
                         </a>
                     <?php } else { ?>
-                        <a href="<?php echo $basePath; ?>register.php" class="text-decoration-none fw-medium" style="color: #607f61;">Sign up</a>
+                        <a href="<?php echo $basePath; ?>register" class="text-decoration-none fw-medium" style="color: #607f61;">Sign up</a>
                         <span class="fw-lighter mx-3" style="color: #2e384d;">|</span>
-                        <a href="<?php echo $basePath; ?>login.php" class="text-decoration-none fw-medium" style="color: #607f61;">Log in</a>
+                        <a href="<?php echo $basePath; ?>login" class="text-decoration-none fw-medium" style="color: #607f61;">Log in</a>
                     <?php } ?>
                 </div>
             </div>
